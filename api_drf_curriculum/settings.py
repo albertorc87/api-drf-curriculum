20fbb855c2509a7705c15245247dcdb57da4a9e0
Alberto <alberto.r.caballero.87@gmail.com>
2020-06-27 10:18:03 +0200

    Modificado uso horario

--- api_drf_curriculum/settings.py
+++ api_drf_curriculum/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
